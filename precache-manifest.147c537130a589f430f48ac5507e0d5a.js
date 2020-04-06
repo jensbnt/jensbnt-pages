fb2b8181193a0b2b56479862b7602c7ceb542ebf
Jens Beernaert <jens.beernaert@student.pxl.be>
2020-04-06 14:34:34 +0200

    build v1

---
+++ precache-manifest.147c537130a589f430f48ac5507e0d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe4875ac1c4b4c4d6e81382b67ed5f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc660eec9d416e1d3ef",
+    "url": "/static/css/main.a0412610.chunk.css"
+  },
+  {
+    "revision": "5e43c89af8aa27015780",
+    "url": "/static/js/2.60048b68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60048b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc660eec9d416e1d3ef",
+    "url": "/static/js/main.3e75a456.chunk.js"
+  },
+  {
+    "revision": "183b5bddfe8ebda1ac98",
+    "url": "/static/js/runtime-main.f9081592.js"
+  }
+]);
