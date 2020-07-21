0d97343cacc9015a4c00decb64120582b1c1e0c0
Jens Beernaert <jens.beernaert@student.pxl.be>
2020-07-21 04:06:22 +0200

    i did an oopsie

---
+++ precache-manifest.e9c4b4dfff4b344f611a24464e4769f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17e0f072cb859aadbf213391230a6de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b9f02c1862b1077e98a",
+    "url": "/static/css/main.b0a8da16.chunk.css"
+  },
+  {
+    "revision": "d097ff5a7eeecf6db554",
+    "url": "/static/js/2.ea50fc83.chunk.js"
+  },
+  {
+    "revision": "7918f9b893310890f0a444ed75c67550",
+    "url": "/static/js/2.ea50fc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9f02c1862b1077e98a",
+    "url": "/static/js/main.d300823c.chunk.js"
+  },
+  {
+    "revision": "183b5bddfe8ebda1ac98",
+    "url": "/static/js/runtime-main.f9081592.js"
+  }
+]);
